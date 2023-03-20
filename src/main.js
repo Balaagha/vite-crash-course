9a056f346b5ac559b613002436f20dc6bbe63348
Balaagha Alihumatov <balaagha.alihumatov@ibar.az>
2023-03-20 12:24:03 +0400

    minor changes

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-import { nanoid } from 'nanoid';
+import sampleImage from './assets/aaa.jpeg';
+import { userName } from './name'
 
-alert("javascript is runnning!" + nanoid())
+console.log(userName)
+
+const img = new Image();
+img.src = sampleImage;
+document.body.appendChild(img);
+
+for (let index = 0; index <= 5; index++) {
+    console.log("index1 =>" + index)
+}
+
+for (let index = 5; index >= 0; index--) {
+    console.log("index2 =>" + index)
+}
